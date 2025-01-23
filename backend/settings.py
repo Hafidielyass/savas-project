b9d3f642244dcc9c4bd63649e9117f60b0c93348
Hafidielyass <elyass.hafdi@gmail.com>
2025-01-23 21:55:51 +0100

    setting up project

--- backend/settings.py
+++ backend/settings.py
@@ -75,12 +75,14 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'savasproject',
+        'USER': 'postgres',
+        'PASSWORD': 'Mypsql1337',
+        'HOST': 'localhost',
+        'PORT': '5432',  # Default PostgreSQL port
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
